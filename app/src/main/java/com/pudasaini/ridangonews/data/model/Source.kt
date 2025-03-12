f8c677f790ba2261d0a771a2db74feb78ecd1b9c
Nirab Pudasaini <developer.nirab@gmail.com>
2025-03-12 14:56:25 +0200

    data class for API response

---
+++ app/src/main/java/com/pudasaini/ridangonews/data/model/Source.kt
@@ -0,0 +1,8 @@
+package com.pudasaini.ridangonews.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Source (
+    @SerializedName("id") val id: String?,
+    @SerializedName("name") val name: String
+)
